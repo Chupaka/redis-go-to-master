a7bc6d813cfb1e8cf86da300a3f98572f58dc491
Pavel Skuratovich <chupaka@gmail.com>
2023-01-09 23:28:29 +0300

    First rewrite of redis-sentinel-proxy to redis-go-to-master

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"errors"
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -13,37 +13,50 @@ import (
 
 var (
 	masterAddr *net.TCPAddr
-	raddr      *net.TCPAddr
-	saddr      *net.TCPAddr
 
-	localAddr    = flag.String("listen", ":9999", "local address")
-	sentinelAddr = flag.String("sentinel", ":26379", "remote address")
-	masterName   = flag.String("master", "", "name of the master redis node")
+	nodes []*net.TCPAddr
+
+	listenAddr = flag.String("listen", "", "listen address:port")
+	redisNodes = flag.String("nodes", "", "comma-separated list of redis nodes")
+	redisAuth  = flag.String("auth", "", "redis auth string")
 )
 
 func main() {
 	flag.Parse()
 
-	laddr, err := net.ResolveTCPAddr("tcp", *localAddr)
-	if err != nil {
-		log.Fatal("Failed to resolve local address: %s", err)
-	}
-	saddr, err = net.ResolveTCPAddr("tcp", *sentinelAddr)
-	if err != nil {
-		log.Fatal("Failed to resolve sentinel address: %s", err)
+	if *listenAddr == "" {
+		log.Fatalln("Must specify listen address!")
 	}
 
-	go master()
+	if *redisNodes == "" {
+		log.Fatalln("Must specify at least one redis node!")
+	}
+
+	laddr, err := net.ResolveTCPAddr("tcp", *listenAddr)
+	if err != nil {
+		log.Fatalf("Failed to resolve listen address: %s\n", err)
+	}
+
+	for _, n := range strings.Split(*redisNodes, ",") {
+		naddr, err := net.ResolveTCPAddr("tcp", n)
+		if err != nil {
+			log.Fatalf("Failed to resolve node address: %s\n", err)
+		}
+
+		nodes = append(nodes, naddr)
+	}
+
+	go followMaster()
 
 	listener, err := net.ListenTCP("tcp", laddr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Can't open listening socket: %s\n", err)
 	}
 
 	for {
 		conn, err := listener.AcceptTCP()
 		if err != nil {
-			log.Println(err)
+			log.Printf("Can't accept connection: %s\n", err)
 			continue
 		}
 
@@ -51,20 +64,20 @@ func main() {
 	}
 }
 
-func master() {
-	var err error
+func followMaster() {
 	for {
-		masterAddr, err = getMasterAddr(saddr, *masterName)
-		if err != nil {
-			log.Println(err)
+		newAddr := getMasterAddr()
+
+		if newAddr == nil {
+			log.Println("No masters found!")
+		} else if masterAddr == nil || string(masterAddr.IP) != string(newAddr.IP) || masterAddr.Port != newAddr.Port {
+			log.Printf("Changing master to %s:%d\n", newAddr.IP, newAddr.Port)
 		}
+
+		masterAddr = newAddr
+
 		time.Sleep(1 * time.Second)
 	}
-}
-
-func pipe(r io.Reader, w io.WriteCloser) {
-	io.Copy(w, r)
-	w.Close()
 }
 
 func proxy(local io.ReadWriteCloser, remoteAddr *net.TCPAddr) {
@@ -75,46 +88,47 @@ func proxy(local io.ReadWriteCloser, remoteAddr *net.TCPAddr) {
 		local.Close()
 		return
 	}
+
 	go pipe(local, remote)
 	go pipe(remote, local)
 }
 
-func getMasterAddr(sentinelAddress *net.TCPAddr, masterName string) (*net.TCPAddr, error) {
-	conn, err := net.DialTCP("tcp", nil, sentinelAddress)
-	if err != nil {
-		return nil, err
+func pipe(r io.Reader, w io.WriteCloser) {
+	io.Copy(w, r)
+	w.Close()
+}
+
+func getMasterAddr() *net.TCPAddr {
+	for _, node := range nodes {
+		d := net.Dialer{Timeout: 1 * time.Second}
+		conn, err := d.Dial("tcp", node.String())
+		if err != nil {
+			continue
+		}
+
+		defer conn.Close()
+
+		if *redisAuth != "" {
+			conn.Write([]byte(fmt.Sprintf("AUTH %s\r\ninfo replication\r\n", *redisAuth)))
+		} else {
+			conn.Write([]byte(fmt.Sprintf("info replication\r\n")))
+		}
+
+		b := make([]byte, 4096)
+
+		l, err := conn.Read(b)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if bytes.Contains(b[:l], []byte("role:master")) {
+			return node
+		}
+
+		if bytes.Contains(b[:l], []byte("-NOAUTH")) {
+			log.Printf("%s: NOAUTH Authentication required\n", node.IP)
+		}
 	}
 
-	defer conn.Close()
-
-	conn.Write([]byte(fmt.Sprintf("sentinel get-master-addr-by-name %s\n", masterName)))
-
-	b := make([]byte, 256)
-	_, err = conn.Read(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	parts := strings.Split(string(b), "\r\n")
-
-	if len(parts) < 5 {
-		err = errors.New("Couldn't get master address from sentinel")
-		return nil, err
-	}
-
-	//getting the string address for the master node
-	stringaddr := fmt.Sprintf("%s:%s", parts[2], parts[4])
-	addr, err := net.ResolveTCPAddr("tcp", stringaddr)
-
-	if err != nil {
-		return nil, err
-	}
-
-	//check that there's actually someone listening on that address
-	conn2, err := net.DialTCP("tcp", nil, addr)
-	if err == nil {
-		defer conn2.Close()
-	}
-
-	return addr, err
+	return nil
 }
